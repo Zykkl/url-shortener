9b980ea7d4301c7a10749bc74e0630cbb954658f
Zykkl <alex@zykkl.dev>
2022-07-17 17:10:06 +0200

    CSS + id check

--- functions/makeId.js
+++ functions/makeId.js
@@ -1,12 +1,23 @@
-function makeid(length) {
+let { Deta } = require('deta')
+let dotenv = require('dotenv')
+
+const deta = Deta(process.env.PROJECT_KEY)
+const db = deta.Base(process.env.DB_NAME)
+dotenv.config()
+
+async function makeId(length) {
 	let result = '';
 	const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
 	const charactersLength = characters.length;
 	for (let i = 0; i < length; i++) {
 		result += characters.charAt(Math.floor(Math.random() *
-charactersLength));
+			charactersLength));
 	}
-	return result;
+	if (await db.get(result) != null) {
+		makeId(length)
+	} else {
+		return result;
+	}
 }
 
-module.exports = { makeid }
+module.exports = { makeId }
--- server.js
+++ server.js
@@ -4,13 +4,12 @@ let app = express();
 let dotenv = require('dotenv');
 dotenv.config()
 
-const { makeid } = require('./functions/makeId.js')
+const { makeId } = require('./functions/makeId.js');
 const deta = Deta(process.env.PROJECT_KEY)
 const db = deta.Base(process.env.DB_NAME)
 
 app.use(express.urlencoded());
 app.set('view engine', 'ejs');
-
 
 app.get('/', (req, res) => {
     res.render('index');
@@ -18,14 +17,16 @@ app.get('/', (req, res) => {
 
 app.post('/api/shorten', async (req, res) => {
     let url = req.body.url
-    let id = makeid(6)
-    await db.put({ url: url, key: id })
-    res.status(200).send('200 OK')
+    let id = makeId(6)
+    db.put({key: id, url: url})
 })
 
 app.get('/:id', async (req, res) => {
     let id = req.params.id
     let url = await db.get(id)
+    if (!url) {
+        return res.status(404).render('404')
+    }
     res.redirect(url.url)
 })
 
